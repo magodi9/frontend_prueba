c2c6cec974c44b0d36a4d6a79ea97fb5e0d7cdcf
Maria Gonzalez <mayigd0502@gmail.com>
2021-11-30 18:07:15 -0500

    First Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <H1>Bienvenidos a la Reserva Forestal de aves</H1>
+    <img src="../assets/aves.jpg" alt="aves" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
